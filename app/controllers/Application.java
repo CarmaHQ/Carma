ff59d49c7898a3de957e90d851be614566e3192b
Anton Podavalov <a.podavalov@gmail.com>
2014-11-03 23:23:03 -0800

    Added showing name of signed in person.

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,8 +1,11 @@
 package controllers;
 
+import com.feth.play.module.pa.PlayAuthenticate;
+import com.feth.play.module.pa.user.AuthUser;
+
+import models.User;
 import play.*;
 import play.mvc.*;
-
 import views.html.*;
 
 @Security.Authenticated(Secured.class)
@@ -11,5 +14,10 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Your new application is ready."));
     }
-
+    
+	public static User getLocalUser() {
+		final AuthUser currentAuthUser = PlayAuthenticate.getUser(session());
+		final User localUser = User.findByAuthUserIdentity(currentAuthUser);
+		return localUser;
+	}
 }
